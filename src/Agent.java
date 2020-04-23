f2a9543fcc7d2c014f93b13f32019e28294d9e07
Théo Dumas <theodesile@gmail.com>
2020-04-23 10:39:05 +0200

    agent update

--- src/Agent.java
+++ src/Agent.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
+import org.cloudbus.cloudsim.network.datacenter.HostPacket;
+import org.cloudbus.cloudsim.network.datacenter.NetworkPacket;
 
-public class Agent {
+public class Agent extends NetworkPacket {
+    public Agent(int id, HostPacket pkt2, int vmid, int cloudletid) {
+        super(id, pkt2, vmid, cloudletid);
+    }
 }
